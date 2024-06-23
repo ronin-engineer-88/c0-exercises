9aa31a6c677509691819837d5fcc2809174382dd
HoangThong37 <thonghoang115@gmail.com>
2024-06-23 17:41:35 +0700

    [CODE] solve homework 09

--- Java Core/src/_09_Exception/homework/_01.java
+++ Java Core/src/_09_Exception/homework/_01.java
@@ -1,6 +1,22 @@
 package _09_Exception.homework;
 
 public class _01 {
+    public static void main(String[] args) {
+        try {
+            m(15);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void m(int value) throws Exception {
+        if (value < 40) {
+            throw new Exception("value is too small");
+        }
+    }
 }
 
 // Homework 01
+
+ /*Vấn đề: method m đang ném ra một exception checked.
+           Mình cần sử dụng từ khóa throws trong khai báo phương thức.*/
--- Java Core/src/_09_Exception/homework/_02.java
+++ Java Core/src/_09_Exception/homework/_02.java
@@ -1,4 +1,42 @@
 package _09_Exception.homework;
 
 public class _02 {
+//    public static void main(String[] args) {
+//        System.out.println(1 / 0);
+//    }
+// => TH này sẽ ném ra ArithmeticException vì chia cho 0.
+
+//    public static void main(String[] args) {
+//        int[] list = new int[5];
+//        System.out.println(list[5]);
+//    }
+// => TH này ném ra ArrayIndexOutOfBoundsException vì chỉ số 5 nằm ngoài giới hạn của mảng (mảng có các chỉ số từ 0 đến 4)
+
+//    public static void main(String[] args) {
+//        String s = "abc";
+//        System.out.println(s.charAt(3));
+//    }
+// => TH này sẽ ném ra StringIndexOutOfBoundsException vì chỉ số 3 nằm ngoài giới hạn của chuỗi (chuỗi có các chỉ số từ 0 đến 2).
+
+//    public static void main(String[] args) {
+//        Object o = new Object();
+//        String d = (String)o;
+//    }
+// => TH này sẽ ném ra ClassCastException vì Object không thể ép kiểu thành String.
+
+//    public static void main(String[] args) {
+//        Object o = null;
+//        System.out.println(o.toString());
+//    }
+// => TH này sẽ ném ra NullPointerException vì gọi phương thức trên đối tượng null.
+
+    public static void main(String[] args) {
+        System.out.println(1.0 / 0);
+    }
+// => TH này sẽ không ném ra exception, trong Java phép chia số thực cho 0 sẽ cho kết quả là Infinity (vô cực),
+//    không ném ra exception
+//
 }
+
+// KẾT LUẬN:  Trường hợp (a), (b), (c), (d), và (e) sẽ ném ra exception vì các lý do đã nêu ở trên.
+//            Trường hợp (f) sẽ không ném ra exception.

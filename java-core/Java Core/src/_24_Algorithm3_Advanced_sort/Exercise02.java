97b9c36ca926d7627b5b1c6b314c1eee6af8a8e5
Dylan <122667165+Pathadyl@users.noreply.github.com>
2024-11-12 08:07:07 +0700

    Complete exercise 2, 4, 6, 8, 10 lecture 24

--- java-core/Java Core/src/_24_Algorithm3_Advanced_sort/Exercise02.java
+++ java-core/Java Core/src/_24_Algorithm3_Advanced_sort/Exercise02.java
@@ -1,35 +1,52 @@
 package _24_Algorithm3_Advanced_sort;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int k = scanner.nextInt();
-
         int[] arr = new int[n];
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
 
-        System.out.println(findx(arr, k));
+        if(checkIfCanSoldInOrder(arr))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+
     }
 
-    private static int findx(int[] arr, int k) {
-        Arrays.sort(arr);
+    private static boolean checkIfCanSoldInOrder(int[] arr) {
+        int count25 = 0;
+        int count50 = 0;
 
-        if(k == 0){
-            if(arr[0] > 1)
-                return 1;
-        } else if (k <= arr.length){
-            if(k == arr.length || arr[k] != arr[k-1])
-                return arr[k-1];
+        for (int bill : arr) {
+            if (bill == 25) {
+                count25++;
+            } else if (bill == 50) {
+                if (count25 > 0) {
+                    count25--;
+                    count50++;
+                } else {
+                    return false;
+                }
+            } else if (bill == 100) {
+                if (count50 > 0 && count25 > 0) {
+                    count50--;
+                    count25--;
+                } else if (count25 >= 3) {
+                    count25 -= 3;
+                } else {
+                    return false;
+                }
+            }
         }
 
-        return -1;
+        return true;
     }
+
 }
 
 
